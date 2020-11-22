875bb8fd6d50aeaaed19ba99ee6087ac8277c382
piotrek123397 <piotrek123397@users.noreply.github.com>
2020-11-22 17:38:10 +0100

    Configure TypeORM.
    refs T47

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,23 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import {TypeOrmModule} from '@nestjs/typeorm';
+import {Module} from '@nestjs/common';
+import {AppController} from './app.controller';
+import {AppService} from './app.service';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+	imports: [
+		TypeOrmModule.forRoot({
+			type: 'pgsql',
+			host: process.env.DB_HOST,
+			port: process.env.DB_PORT,
+			username: process.env.DB_USERNAME,
+			password: process.env.DB_PASSWORD,
+			database: process.env.DB_DATABASE,
+			entities: [],
+			synchronize: process.env.NODE_ENV !== 'production',
+		}),
+	],
+	controllers: [AppController],
+	providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+}
