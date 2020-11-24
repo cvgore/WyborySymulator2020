d8c26b4f6a657dee020bf23018f07393f73cf110
Kacper <kacper@cvgo.re>
2020-11-25 00:43:23 +0100

    Fix type orm module config
    Refs T47

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,21 +1,22 @@
-import {TypeOrmModule} from '@nestjs/typeorm';
-import {Module} from '@nestjs/common';
-import {AppController} from './app.controller';
-import {AppService} from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 @Module({
 	imports: [
 		TypeOrmModule.forRoot({
-			type: 'pgsql',
+			type: 'postgres',
 			host: process.env.DB_HOST,
-			port: process.env.DB_PORT,
+			port: Number(process.env.DB_PORT),
 			username: process.env.DB_USERNAME,
 			password: process.env.DB_PASSWORD,
 			database: process.env.DB_DATABASE,
 			entities: [],
 			synchronize: process.env.NODE_ENV !== 'production',
 		}),
-	],controllers: [AppController],
+	],
+	controllers: [AppController],
 	providers: [AppService],
 })
 export class AppModule {
