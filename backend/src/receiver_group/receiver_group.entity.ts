5fe0fe99abc501bd24d03b786fa7d3d9c70e1d92
piotrek123397 <piotrek123397@users.noreply.github.com>
2020-11-27 19:31:55 +0100

    Create Receiver Group Table
    refs T40
    closes

---
+++ backend/src/receiver_group/receiver_group.entity.ts
@@ -0,0 +1,15 @@
+import {BeforeInsert, BeforeUpdate, Column, Entity, OneToOne, PrimaryGeneratedColumn} from 'typeorm';
+import {UserEntity} from "../user/user.entity";
+
+
+@Entity('receiver_group')
+export class Receiver_groupEntity{
+	@PrimaryGeneratedColumn()
+	id!: number;
+	@OneToOne(type => UserEntity)
+	user!: UserEntity;
+	@Column()
+	name!: string;
+
+}
+
