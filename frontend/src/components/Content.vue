977e235ed71d017fccede1ae45e864dc5ede7cd2
Adrian <adrian.pykacz48@gmail.com>
2020-12-01 23:13:24 +0100

    Add main content button styling

--- frontend/src/components/Content.vue
+++ frontend/src/components/Content.vue
@@ -5,6 +5,16 @@
     </p>
     <section class="cards-container">
       <QuizCard/>
+      <QuizCard/>
+      <QuizCard/>
+      <QuizCard/>
+    </section>
+    <section class="button-area">
+      <button class="active-btn">
+      <span>
+        Start Quiz
+      </span>
+      </button>
     </section>
   </section>
 </template>
@@ -21,10 +31,30 @@ export default {
 </script>
 
 <style scoped>
+.button-area {
+  margin-top: 30px;
+  display: flex;
+  justify-content: center;
+}
+
 .cards-container {
   display: grid;
   grid-template-columns: repeat(4, 1fr);
   grid-gap: 20px;
+}
+
+.active-btn {
+  font-weight: 700;
+  width: 200px;
+  height: 50px;
+  color: hsl(0, 0%, 23%);
+  display: flex;
+  justify-content: center;
+  border: 0;
+  align-items: center;
+  background-color: hsl(0, 41%, 88%);
+  border-radius: 30px;
+  font-size: 1.3em;
 }
 
 .pager {
