ebfb7ec7eb033f2dd61912edaa0af70959114ff3
piotrek123397 <piotrwaliczek2@gmail.com>
2020-12-03 15:50:01 +0100

    Create poll option table Refs T33

---
+++ backend/src/poll_option/poll_option.entity.ts
@@ -0,0 +1,32 @@
+import {BeforeInsert, BeforeUpdate, Column, Entity, OneToOne, PrimaryGeneratedColumn} from 'typeorm';
+import {IsDate} from "class-validator";
+
+
+@Entity()
+export class Receivers{
+	@PrimaryGeneratedColumn()
+	id!: number;
+	@Column()
+	poll_id!: number;
+	@Column()
+	name!: string;
+
+	@Column({type: 'timestamp'})
+	@IsDate()
+	createdAt!: Date;
+
+	@Column({type: 'timestamp'})
+	@IsDate()
+	updatedAt!: Date;
+
+	@BeforeInsert()
+	setCreatedAt() {
+		this.createdAt = new Date();
+	}
+
+	@BeforeInsert()
+	@BeforeUpdate()
+	setUpdatedAt() {
+		this.updatedAt = new Date();
+	}
+}
