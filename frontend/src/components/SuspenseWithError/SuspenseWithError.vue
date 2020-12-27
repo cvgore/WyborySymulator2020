0b79c09933cbbc3fbda7339b2898f1936d6a3a2a
Adrian <adrian.pykacz48@gmail.com>
2020-12-27 17:53:41 +0100

    config suspense component

--- frontend/src/components/SuspenseWithError/SuspenseWithError.vue
+++ frontend/src/components/SuspenseWithError/SuspenseWithError.vue
@@ -1,13 +1,29 @@
 <template>
-
+  <slot v-if="" name="error"></slot>
+  <Suspense>
+    <template #default>
+      <slot name="default"></slot>
+    </template>
+    <template #fallback>
+      <slot name="fallback"></slot>
+    </template>
+  </Suspense>
 </template>
 
 <script>
+import { ref, onErrorCaptured } from 'vue';
+
 export default {
-name: "SuspenseWithError"
-}
+  name: 'SuspenseWithError',
+  setup(){
+    const error = ref(null);
+    onErrorCaptured((e) => {
+      error.value = e
+      return true;
+    })
+    return {error}
+  }
+};
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
