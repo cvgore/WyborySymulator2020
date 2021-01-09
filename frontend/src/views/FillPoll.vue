8cc2a000103d447a75ea72774caa79ffc8ff0e1a
vavelysh <adrian.pykacz48@gmail.com>
2021-01-09 13:49:43 +0100

    rysiek do roboty

---
+++ frontend/src/views/FillPoll.vue
@@ -0,0 +1,18 @@
+<template>
+  <MarginatorTopator> siema {{ id }} </MarginatorTopator>
+</template>
+
+<script>
+import MarginatorTopator from '@/components/UI/MarginatorTopator';
+export default {
+  name: 'FillPoll',
+  components: { MarginatorTopator },
+  computed: {
+    id() {
+      return this.$route.params.id;
+    },
+  },
+};
+</script>
+
+<style scoped></style>
