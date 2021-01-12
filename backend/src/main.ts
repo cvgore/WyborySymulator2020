acca7b58cba890baa345a1912112a5215e5bd668
Kacper <kacper@cvgo.re>
2021-01-12 19:40:44 +0100

    change default port

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,7 +6,7 @@ import {AppModule} from './app.module';
 // Creates an application and runs it asynchronously
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
-	await app.listen(3000);
+	await app.listen(3850);
 
 	// FIXME: Enable hot reload when https://github.com/ericclemmons/start-server-webpack-plugin/issues/40 is resolved
 	// if (module.hot) {
