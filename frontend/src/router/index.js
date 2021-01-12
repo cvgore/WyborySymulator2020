5a3debb623e0d07f873aa0048fb52dd84b0a15e7
vavelysh <adrian.pykacz48@gmail.com>
2021-01-12 20:32:16 +0100

    usuwanie syfu

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,16 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
 const routes = [
-  {
-    path: '/',
-    name: "Home",
-    component: () => import("@/views/Surveys"),
-  },
-  {
-    path: "/polls/:id",
-    name: "poll",
-    component: () => import("@/views/FillPoll")
-  }
+
 ];
 
 const router = createRouter({
