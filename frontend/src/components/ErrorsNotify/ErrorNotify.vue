b04f45af1a8e2558f2e6c726002ad533f97a7412
Dawid Zelinka <dawid+gcsh@cvgo.re>
2021-01-27 16:40:36 +0000

    cos

--- frontend/src/components/ErrorsNotify/ErrorNotify.vue
+++ frontend/src/components/ErrorsNotify/ErrorNotify.vue
@@ -1,7 +1,26 @@
 <template>
-  <div class="notification is-danger">
-    Serwer nie jest włączony
+  
+<section class="hero is-medium is-primary is-bold">
+  <div class="hero-body">
+    <div class="container">
+      <h1 class="title">
+        Kiedyś to sie zrobi
+      </h1>
+    </div>
   </div>
+</section>
+
+<article class="message is-primary">
+  <div class="message-header">
+    <p>Primary</p>
+    <button class="delete" aria-label="delete"></button>
+  </div>
+
+  <div class="message-body">
+     <strong>Czeka nas smutny koniec ;(</strong> 
+  </div>
+</article>
+
 </template>
 
 <script>
