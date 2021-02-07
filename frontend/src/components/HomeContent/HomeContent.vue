27428f5fdd8c7c5c8398b33cc26680b604dcf686
Adrian P <adrian.pykacz48@gmail.com>
2021-02-07 22:57:01 +0100

    start creating poll view

--- frontend/src/components/HomeContent/HomeContent.vue
+++ frontend/src/components/HomeContent/HomeContent.vue
@@ -4,32 +4,28 @@
       <img src="@/assets/man.png" alt="icon">
     </figure>
     <p class="title is-size-2  has-text-info">Nie jesteś zalogowany!</p>
-    <router-link to="/log-in" class="button is-link is-large">Zaloguj się</router-link>
+    <router-link to="/forms/log-in" class="button is-link is-large">Zaloguj się</router-link>
+  </div>
+  <div v-else>
+    <ListPolls />
   </div>
 </template>
 
 <script>
 import Navigation from '@/components/Navigation/Navigation';
-import Otwarte from '@/components/PollViews/Open';
-import Zamkniete from '@/components/PollViews/Closed';
 import Layout from '@/components/Layout';
 import {mapActions, mapState} from 'vuex';
+import ListPolls from "@/components/ListPolls/ListPolls";
 
 export default {
   name: 'HomeContent',
   components: {
+    ListPolls,
     Layout,
     Navigation,
   },
-  methods: {
-    ...mapActions('Polls',['fetchPolls']),
-  },
   computed: {
-    ...mapState('Polls',['isError']),
     ...mapState('Auth',['isAuth']),
-  },
-  async created() {
-    await this.fetchPolls();
   },
 };
 </script>
---
+++ frontend/src/components/ListPolls/ListPolls.vue
@@ -0,0 +1,38 @@
+<template>
+  <div v-if="!isAuth" class="boxx section is-flex is-flex-direction-column is-justify-content-center is-align-items-center container">
+    <figure class="image is-128x128 my-3">
+      <img src="@/assets/happy-man.png" alt="icon hm">
+    </figure>
+    <div class="columns is-centered">
+      <div class="column">
+        <p class="title is-size-4 has-text-info">Ahoj, {{onlyName}}</p>
+      </div>
+    </div>
+    <p class="subtitle is-size-5 has-text-info">Oto twoje ankiety:</p>
+    <p>siezrobi</p>
+  </div>
+</template>
+
+<script>
+import {mapActions, mapState} from "vuex";
+
+export default {
+  name: "ListPolls",
+  async created() {
+    await this.fetchPolls();
+  },
+  computed: {
+    ...mapState('Auth',['email']),
+    onlyName(){
+      return this.email.substring(0,this.email.indexOf('@'));
+    }
+  },
+  methods: {
+    ...mapActions('Polls',['fetchPolls']),
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
