b830ba00dc02d6c0212afe1c0fd346f3999c868a
Dawid Zelinka <dawid+gcsh@cvgo.re>
2021-02-10 19:09:06 +0000

    confirm.vue changes

--- frontend/src/components/Auth/Confirm.vue
+++ frontend/src/components/Auth/Confirm.vue
@@ -52,7 +52,7 @@ import {useRouter} from "vue-router";
 import {usePost} from "@/utils/usePost";
 import { Form,Field,ErrorMessage } from 'vee-validate';
 import yup from "@/yup-settings";
-
+import axios from "@/axios";
 export default {
   name: "Confirm",
   components: { Form,Field,ErrorMessage },
@@ -79,18 +79,17 @@ export default {
       const objJsonB64 = atob(state.token);
       const decode = JSON.parse(objJsonB64);
       const response = await usePost('/user/login/authorize', {
-        email: store.state.Auth.email,
+        email: window.localStorage.getItem('email'),
         token: decode.token,
         ts: decode.ts,
       });
       state.apiData = response;
       state.token = '';
       if(response.statusCode === 201){
-        store.commit('Auth/insertToken',{
-          token: response.data.access_token
-        });
-        store.commit('Auth/changeAuth',true);
-
+        window.localStorage.setItem('token', response.data.access_token);
+        window.localStorage.setItem('status', 'logged');
+        axios.defaults.headers.common['Authorization'] = `Bearer ${window.localStorage.getItem('token')}`;
+        await store.dispatch('Auth/checkAuth');
         await router.replace('/');
       }
     }
@@ -101,4 +100,3 @@ export default {
   }
 }
 </script>
-
