6f6541301e6244be1a055798d411fcfce469b5a2
Dawid Zelinka <dawid+gcsh@cvgo.re>
2021-02-10 19:15:01 +0000

    NotifyBox

---
+++ frontend/src/components/ListPolls/NotifyBox.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <section v-if="errors.delete">
+      <div v-if="errors.delete.status" class="notification is-danger">
+        <button class="delete" @click="closeNotify"/>
+        Błąd podczas usuwania ankiety
+        <p class="is-size-6">
+          {{ state.errors.delete.message }}
+        </p>
+      </div>
+    </section>
+    <section v-if="errors.publish">
+      <div v-if="errors.publish.status" class="notification is-danger">
+        <button class="delete" @click="closeNotify"/>
+        Błąd podczas publikowania ankiety
+        <p class="is-size-6">
+          {{ state.errors.publish.message }}
+        </p>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NotifyBox",
+  props: {
+    errors: Object
+  },
+  methods: {
+    closeNotify() {
+      this.errors = null;
+    }
+  }
+}
+</script>
