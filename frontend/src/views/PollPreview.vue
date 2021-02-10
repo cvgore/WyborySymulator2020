6462cdb2883accfe4df904a6d7b9d380b821b02a
Dawid Zelinka <dawid+gcsh@cvgo.re>
2021-02-10 19:23:42 +0000

    PollPreview changes

--- frontend/src/views/PollPreview.vue
+++ frontend/src/views/PollPreview.vue
@@ -3,15 +3,14 @@
     <div class="hero-body">
       <div class="container">
         <h1 class="title">
-          {{state.pickedPoll.name}}
+<!--          {{state.pickedPoll.name}}-->
         </h1>
       </div>
     </div>
   </section>
-  <QuestionList :picked-poll-data="state.pickedPoll"/>
+<!--  <QuestionList :picked-poll-data="state.pickedPoll"/>-->
 </template>
 <script>
-
 import QuestionList from "@/components/QuestionList/QuestionList";
 import SuspenseWithError from "@/components/SuspenseWithError/SuspenseWithError";
 import ErrorNotify from "@/components/ErrorsNotify/ErrorNotify";
@@ -26,11 +25,14 @@ export default {
     const store = useStore();
     const state = reactive({
       pickedPoll:null,
-      id: null
+      id: null,
+      str: null
     });
     state.id = +route.params.id;
-    const getPoll = store.getters['Polls/getPollById'];
-    state.pickedPoll = getPoll(state.id);
+    state.str = route.params.str;
+    console.log(route)
+    // const getPoll = store.getters['Polls/getPollById'];
+    // state.pickedPoll = getPoll(state.id);
     return {
       state,
     }
