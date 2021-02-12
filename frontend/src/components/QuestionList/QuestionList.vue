14b8b6021da1d58d52ae341cc227afa3e652eda8
Dawid Zelinka <dawid+gcsh@cvgo.re>
2021-02-12 17:09:13 +0000

    QuestionList

--- frontend/src/components/QuestionList/QuestionList.vue
+++ frontend/src/components/QuestionList/QuestionList.vue
@@ -149,6 +149,7 @@ export default {
           const [res] = await Promise.all([axios.post(`/poll/${props.id}/${props.str}/vote`, ans)]).then(d => {
             state.isSuccess = true;
           })
+          console.log(res)
         }
       } catch {
         state.isError = true;
